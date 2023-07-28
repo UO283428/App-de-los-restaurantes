2c62bc78835fdfd6f9afb370ffce763f651d1e0d
boldviejo <hola@guillermoviejo.com>
2023-07-28 21:49:57 +0200

    Rating main page styling finished.

--- upRate/frontRate/src/main-rating/Rating.js
+++ upRate/frontRate/src/main-rating/Rating.js
@@ -5,6 +5,7 @@ import Text from './components/Text';
 import CustomStarIcon from './components/CustomStarIcon';
 import StarRatings from 'react-star-ratings';
 
+
 const Rating = () => {
 
   const [rating, setRating] = useState(0);
@@ -19,9 +20,10 @@ const Rating = () => {
           <form onSubmit={handleSubmit} className="review-form">
               {/*<Image className="background-image" src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />*/}
               <Image className="front-page-image" src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
-              <Logo className="logo-image" src="https://retailres.com/wp-content/uploads/2020/10/GOIKO-1-1024x465.png" alt="Example Logo" />
+              <Logo className="logo-image"
+              src="http://boldflowco.com/wp-content/uploads/2023/07/logo-goiko.png" alt="Example Logo" />
               <div className="rating-container">
-                <Text className="text-content" content="Rate the restaurant" />
+                <Text className="text-content" content="¿Que te ha parecido el ser?" />
                 <CustomStarIcon className="rating-icons" setRating={setRating}/>
                 <input type="submit" className="submit-button" value="Enviar reseña" />
               </div>
