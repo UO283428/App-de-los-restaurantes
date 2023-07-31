821e2cbf3cb1d01c0b81c57d6a0cb83eb728fdf7
Carlos Triana <uo283428@uniovi.es>
2023-07-31 19:50:52 +0200

    Continue working

--- upRate/frontRate/src/main-rating/Rating.js
+++ upRate/frontRate/src/main-rating/Rating.js
@@ -9,7 +9,8 @@ import CustomStarIcon from './components/CustomStarIcon';
 const Rating = () => {
   // State definition
   const [rating, setRating] = useState(0);
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isReviewing, setIsReviewing] = useState(false);
+  const [isAsking, setIsAsking] = useState(false);
   const [mainText, setMainText] = useState("¿Cómo calificarías tu experiencia?");
 
   // Handles form submit event
@@ -24,46 +25,165 @@ const Rating = () => {
 
   // Changes UI elements based on the rating value
   const changeElements = () => {
-    setIsExpanded(true);
-    setMainText("¿Qué podemos mejorar?");
-
+    if (isReviewing === false && isAsking === false) {
+      setIsReviewing(true);
+      setMainText("¿Qué podemos mejorar?");
+    }
+    else if (isReviewing === true && isAsking === false) {
+      setIsReviewing(false);
+      setIsAsking(true);
+      setMainText("¿Qué podemos mejorar?");
+    }
   };
 
-  return (
-    <div className="app">
-      <form onSubmit={handleSubmit} className="review-form">
-        <Image 
-          className= {'front-page-image' + (isExpanded ? ' expanded' : '')} 
-          src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
-        
-        <Logo 
-          className={"logo-image" + (isExpanded ? ' expanded' : '')} 
-          src="https://boldflowco.com/wp-content/uploads/2023/07/logo-goiko.png" alt="Example Logo" />
-        
-        <div 
-          className={"rating-container" + (isExpanded ? ' expanded' : '')}>
+  // function that returns all the elements form the first page
+  const mainPage = () => {
+    return(
+      <div className="app">
+        <form onSubmit={handleSubmit} className="review-form">
+          <Image 
+            className= {'front-page-image' + (isReviewing ? ' reviewing' : '')} 
+            src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
           
-          <Text 
-            className={"text-content" + (isExpanded ? ' expanded' : '')}
-            content={mainText} />
+          <Logo 
+            className={"logo-image" + (isReviewing ? ' reviewing' : '')} 
+            src="https://boldflowco.com/wp-content/uploads/2023/07/logo-goiko.png" alt="Example Logo" />
           
-          <CustomStarIcon 
-            className={"rating-icons" + (isExpanded ? ' expanded' : '')}
-            setRating={setRating}/>
+          <div 
+            className={"rating-container" + (isReviewing ? ' reviewing' : '')}>
+            
+            <Text 
+              className={"text-content" + (isReviewing ? ' reviewing' : '')}
+              content={mainText} />
+            
+            <CustomStarIcon 
+              className={"rating-icons" + (isReviewing ? ' reviewing' : '')}
+              setRating={setRating}/>
 
-          {/* If the rating is less than 3, show the input of text.*/}
+            <input 
+              type="submit"
+              className={"submit-button" + (isReviewing ? ' reviewing' : '')}
+              value="Enviar reseña" />
+          </div>
+        </form>
+      </div>
+    ); 
+ };
+
+  // function that returns all the elements form the second page
+  const reviewingPage = () => {
+    return (
+      <div className="app">
+        <form onSubmit={handleSubmit} className="review-form">
+          <Image 
+            className= {'front-page-image' + (isReviewing ? ' reviewing' : '')} 
+            src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
+          
+          <Logo 
+            className={"logo-image" + (isReviewing ? ' reviewing' : '')} 
+            src="https://boldflowco.com/wp-content/uploads/2023/07/logo-goiko.png" alt="Example Logo" />
+          
+          <div 
+            className={"rating-container" + (isReviewing ? ' reviewing' : '')}>
+            <div
+              className={"rating-question " + (isReviewing ? ' reviewing' : '')}>
+              <Text 
+                className={"text-content" + (isReviewing ? ' reviewing' : '')}
+                content={mainText} />
+              
+              <CustomStarIcon 
+                className={"rating-icons" + (isReviewing ? ' reviewing' : '')}
+                setRating={setRating}/>
+            </div>
+
+            <div
+              className={"rating-question " + (isReviewing ? ' reviewing' : '')}>
+              <Text 
+                className={"text-content" + (isReviewing ? ' reviewing' : '')}
+                content={mainText} />
+              
+              <CustomStarIcon 
+                className={"rating-icons" + (isReviewing ? ' reviewing' : '')}
+                setRating={setRating}/>
+            </div>
+
+            <div
+              className={"rating-question " + (isReviewing ? ' reviewing' : '')}>
+              <Text 
+                className={"text-content" + (isReviewing ? ' reviewing' : '')}
+                content={mainText} />
+              
+              <CustomStarIcon 
+                className={"rating-icons" + (isReviewing ? ' reviewing' : '')}
+                setRating={setRating}/>
+            </div>
+
+
+            {/* If the rating is less than 3, show the input of text.*/}
+            <input
+              className={"text-input" + (isReviewing ? ' reviewing' : '')}
+              type="text"
+              placeholder="Escribe tu opinión aquí"
+            />
+
+            <input 
+              type="submit"
+              className={"submit-button" + (isReviewing ? ' reviewing' : '')}
+              value="Enviar respuestas" />
+          </div>
+        </form>
+      </div>
+    );
+  };
+
+    // function that returns all the elements form the second page
+    // TODO: Asking page not working
+    const askingPage = () => {
+      return (
+        <div className="app">
+          <form onSubmit={handleSubmit} className="review-form">
+            <Image 
+              className= {'front-page-image' + (isReviewing ? ' reviewing' : '')} 
+              src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
+            
+            <Logo 
+              className={"logo-image" + (isReviewing ? ' reviewing' : '')} 
+              src="https://boldflowco.com/wp-content/uploads/2023/07/logo-goiko.png" alt="Example Logo" />
+            
+            <div 
+              className={"rating-container" + (isReviewing ? ' reviewing' : '')}>
+                <Text 
+                  className={"text-content" + (isReviewing ? ' reviewing' : '')}
+                  content={mainText} />
+                
+  
+              {/* If the rating is less than 3, show the input of text.*/}
+              <input
+                className={"text-input" + ""}
+                type="text"
+                placeholder="aaaaaaaaaaquí"
+              />
+  
+              <input 
+                type="submit"
+                className={"submit-button" + (isReviewing ? ' reviewing' : '')}
+                value="Enviar respuestas" />
+            </div>
+          </form>
           <input
-            className={"text-input" + (isExpanded ? ' expanded' : '')}
-            type="text"
-            placeholder="Escribe tu opinión aquí"
-          />
-
-          <input 
-            type="submit"
-            className={"submit-button" + (isExpanded ? ' expanded' : '')}
-            value="Enviar reseña" />
+                className={"text-input" + ""}
+                type="text"
+                placeholder="aaaaaaaaaaquí"
+              />
         </div>
-      </form>
+      );
+    };
+
+  return (
+    <div>
+      { !isReviewing && !isAsking ? mainPage() : '' }
+      { isReviewing && !isAsking ? reviewingPage() : '' }
+      { !isReviewing && isAsking ? askingPage() : ''}
     </div>
   );
 };
