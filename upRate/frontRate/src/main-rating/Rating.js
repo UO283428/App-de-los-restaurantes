ad2ac26de2fe1ccc55d9f55815d2bcb82cf547ae
Carlos Triana <uo283428@uniovi.es>
2023-07-31 18:29:37 +0200

    First page finished.

--- upRate/frontRate/src/main-rating/Rating.js
+++ upRate/frontRate/src/main-rating/Rating.js
@@ -1,33 +1,72 @@
-import React, { useState } from 'react';
+// Importing required modules and components
+import React, { useState, useEffect } from 'react';
 import Image from './components/Image';
 import Logo from './components/Logo';
 import Text from './components/Text';
 import CustomStarIcon from './components/CustomStarIcon';
-import StarRatings from 'react-star-ratings';
 
+// Rating Component
 const Rating = () => {
-
+  // State definition
   const [rating, setRating] = useState(0);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [mainText, setMainText] = useState("¿Cómo calificarías tu experiencia en Goiko?");
 
+  // Handles form submit event
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    alert(`Submitting Rating ${rating}`)
-  }
+    if (rating > 3) {
+      alert(`Thank you for your review!`);
+    } else {
+      changeElements();
+    }
+  };
 
-    return (
-      <div className="app">
-          <form onSubmit={handleSubmit} className="review-form">
-              {/*<Image className="background-image" src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />*/}
-              <Image className="front-page-image" src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
-              <Logo className="logo-image" src="https://retailres.com/wp-content/uploads/2020/10/GOIKO-1-1024x465.png" alt="Example Logo" />
-              <div className="rating-container">
-                <Text className="text-content" content="Rate the restaurant" />
-                <CustomStarIcon className="rating-icons" setRating={setRating}/>
-                <input type="submit" className="submit-button" value="Enviar reseña" />
-              </div>
-          </form>
-      </div>
+  // Changes UI elements based on the rating value
+  const changeElements = () => {
+    setIsExpanded(true);
+    setMainText("¿Qué podemos mejorar?");
+
+  };
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit} className="review-form">
+        <Image 
+          className= {'front-page-image' + (isExpanded ? ' expanded' : '')} 
+          src="https://www.justretail.news/wp-content/uploads/2021/08/Goiko-H2O-noticias-retail-2-scaled.jpg" alt="Example Image" />
+        
+        <Logo 
+          className={"logo-image" + (isExpanded ? ' expanded' : '')} 
+          src="https://boldflowco.com/wp-content/uploads/2023/07/logo-goiko.png" alt="Example Logo" />
+        
+        <div 
+          className={"rating-container" + (isExpanded ? ' expanded' : '')}>
+          
+          <Text 
+            className={"text-content" + (isExpanded ? ' expanded' : '')}
+            content={mainText} />
+          
+          <CustomStarIcon 
+            className={"rating-icons" + (isExpanded ? ' expanded' : '')}
+            setRating={setRating}/>
+
+          {/* If the rating is less than 3, show the input of text.*/}
+          <input
+            className={"text-input" + (isExpanded ? ' expanded' : '')}
+            type="text"
+            placeholder="Escribe tu opinión aquí"
+          />
+
+          <input 
+            type="submit"
+            className={"submit-button" + (isExpanded ? ' expanded' : '')}
+            value="Enviar reseña" />
+        </div>
+      </form>
+    </div>
   );
 };
 
+// Exporting Rating component as default
 export default Rating;
